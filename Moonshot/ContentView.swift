20a4e9bbeddbbcea1cc0ffdbb46bfe96b19e5289
IhorSukhachov <i.sukhachov@gmail.com>
2025-11-20 18:09:54 +0200

    scrollview showcase

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -9,20 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(.example)
-                .resizable()
-                .scaledToFit()
-//                .scaledToFill()
-//                .frame(width: 300, height: 500)
-                .containerRelativeFrame(.horizontal) { size, axis in
-                    size * 0.8
+//        VStack {
+//            Image(.example)
+//                .resizable()
+//                .scaledToFit()
+////                .scaledToFill()
+////                .frame(width: 300, height: 500)
+//                .containerRelativeFrame(.horizontal) { size, axis in
+//                    size * 0.8
+//                }
+//                
+//                
+//               
+//        }
+        ScrollView {
+            VStack(spacing: 10) {
+                ForEach(0..<100) {
+                    Text("Item \($0)").font(.title)
                 }
-                
-                
-               
+            }
         }
-       
     }
 }
 
