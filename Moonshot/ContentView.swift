c7235108ef1a740ea3dec09e2d7b0b63b0e483ff
IhorSukhachov <i.sukhachov@gmail.com>
2025-11-20 19:13:17 +0200

    using grid columns

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -7,35 +7,53 @@
 
 import SwiftUI
 
-struct User: Codable {
-    let name: String
-    let adress: Address
-}
-
-struct Address: Codable {
-    let street: String
-    let city: String
-}
+//struct User: Codable {
+//    let name: String
+//    let adress: Address
+//}
+//
+//struct Address: Codable {
+//    let street: String
+//    let city: String
+//}
 
 struct ContentView: View {
+let layout = [
+    GridItem(.fixed(80)),
+    GridItem(.fixed(80)),
+    GridItem(.fixed(80))
+]
+
     var body: some View {
-        Button("DEcode JSON") {
-            let input = """
-                {
-                    "name" : "Taylor Swift",
-                    "adress" : {
-                            "street" : "555 Zero Street",
-                            "city" : "Nashville"
-                                }
+        ScrollView {
+            LazyVGrid(columns: layout) {
+                ForEach(0..<1000) {
+                    Text("item \($0)")
                 }
-                """
-            let data = Data(input.utf8)
-            let decoder = JSONDecoder()
-            
-            if let user = try? decoder.decode(User.self, from: data) {
-                print(user.adress.street)
             }
         }
+        
+        
+        
+        
+        
+        //        Button("DEcode JSON") {
+//            let input = """
+//                {
+//                    "name" : "Taylor Swift",
+//                    "adress" : {
+//                            "street" : "555 Zero Street",
+//                            "city" : "Nashville"
+//                                }
+//                }
+//                """
+//            let data = Data(input.utf8)
+//            let decoder = JSONDecoder()
+//            
+//            if let user = try? decoder.decode(User.self, from: data) {
+//                print(user.adress.street)
+//            }
+//        }
     }
 }
 
