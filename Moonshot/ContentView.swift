37661813d0db1b7523f533d0a5e6282d3a0e9443
IhorSukhachov <i.sukhachov@gmail.com>
2025-11-20 18:39:49 +0200

    using navigation links with liists

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -11,18 +11,23 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         NavigationStack {
-            NavigationLink {
-                Text("Detailed view")
-            }
-            label: {
-                VStack {
-                    Text("This is a label")
-                    Text("This is also a lable")
-                    Image(systemName: "star")
-                        
-                }.font(.largeTitle)
-            }
-                .navigationTitle("Moonshot")
+//            NavigationLink {
+//                Text("Detailed view")
+//            }
+//            label: {
+//                VStack {
+//                    Text("This is a label")
+//                    Text("This is also a lable")
+//                    Image(systemName: "star")
+//                        
+//                }.font(.largeTitle)
+//            }
+//
+            List(0..<100) {row in
+                NavigationLink("Row \(row)") {
+                    Text("Detail \(row)")
+                }
+            } .navigationTitle("Moonshot")
         }
     }
 }
