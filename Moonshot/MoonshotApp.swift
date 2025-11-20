33ce377044dafaef0430aebe8ffb8d41f5d1ab3f
IhorSukhachov <i.sukhachov@gmail.com>
2025-11-20 14:14:16 +0200

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Ihor Sukhachov on 20.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
