e1dca500bd7b58f0370c995e1d7d000f401a31a9
IhorSukhachov <i.sukhachov@gmail.com>
2025-11-21 14:59:29 +0200

    making main scrollview

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -11,10 +11,39 @@ struct ContentView: View {
     let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
     let missions: [Mission] = Bundle.main.decode("missions.json")
     
+    let columns = [
+        GridItem(.adaptive(minimum: 150))
+    ]
+    
     var body: some View {
-        Text(String(astronauts.count))
-    }
-}
+        NavigationStack {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(missions) { mission in
+                        NavigationLink {
+                            Text("Detailed view")
+                        } label: {
+                            VStack {
+                                Image(mission.image)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 100, height: 100)
+                                Text(mission.displayName)
+                                    .font(.headline)
+                                Text(mission.launchDate ?? "N/A")
+                                    .font(.caption)
+                            }
+                            .frame(maxWidth: .infinity)
+                        }
+                    }
+                }
+                
+            }
+            
+                }
+            }
+        }
+    
 
 #Preview {
     ContentView()
