ad40322b2cd9632c1a296e5ba642096ca01b4a0f
IhorSukhachov <i.sukhachov@gmail.com>
2025-11-21 15:19:32 +0200

    adding date computed property

--- Moonshot/Mission.swift
+++ Moonshot/Mission.swift
@@ -15,7 +15,7 @@ struct Mission: Codable, Identifiable {
         let role: String
     }
     let id: Int
-    let launchDate: String?
+    let launchDate: Date?
     let crew: [CrewRole]
     let description: String
     
@@ -25,6 +25,10 @@ struct Mission: Codable, Identifiable {
     
     var image: String {
         "apollo\(id)"
+    }
+    
+    var formattedLaunchDate: String {
+        launchDate?.formatted(date: .abbreviated, time: .omitted) ?? "N/A"
     }
 }
  
