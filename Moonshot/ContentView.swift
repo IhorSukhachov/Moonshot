488e90a4cbda9232e70eb3e22365d71dfebab943
IhorSukhachov <i.sukhachov@gmail.com>
2025-11-21 10:05:41 +0200

    better way to catch error in json decoding

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -7,54 +7,11 @@
 
 import SwiftUI
 
-//struct User: Codable {
-//    let name: String
-//    let adress: Address
-//}
-//
-//struct Address: Codable {
-//    let street: String
-//    let city: String
-//}
-
 struct ContentView: View {
-let layout = [
-//    GridItem(.fixed(80)),
-//    GridItem(.fixed(80)),
-//    GridItem(.fixed(80))
-    GridItem(.adaptive(minimum: 80))
-]
-
+    let astronauts = Bundle.main.decode("astronauts.json")
+    
     var body: some View {
-        ScrollView {
-            LazyVGrid(columns: layout) {
-                ForEach(0..<1000) {
-                    Text("item \($0)")
-                }
-            }
-        }
-        
-        
-        
-        
-        
-        //        Button("DEcode JSON") {
-//            let input = """
-//                {
-//                    "name" : "Taylor Swift",
-//                    "adress" : {
-//                            "street" : "555 Zero Street",
-//                            "city" : "Nashville"
-//                                }
-//                }
-//                """
-//            let data = Data(input.utf8)
-//            let decoder = JSONDecoder()
-//            
-//            if let user = try? decoder.decode(User.self, from: data) {
-//                print(user.adress.street)
-//            }
-//        }
+        Text(String(astronauts.count))
     }
 }
 
