57c45073da907b461547f4808153100d65ea9bc0
Ihor Sukhachov <i.sukhachov@gmail.com>
2025-11-22 21:40:09 +0200

    new mission view

---
+++ Moonshot/MissionView.swift
@@ -0,0 +1,18 @@
+//
+//  MissionView.swift
+//  Moonshot
+//
+//  Created by Ihor Sukhachov on 22.11.2025.
+//
+
+import SwiftUI
+
+struct MissionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MissionView()
+}
