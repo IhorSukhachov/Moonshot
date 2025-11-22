c99cafe2a9cfc30c8145e72065a74b43b67b1496
Ihor Sukhachov <i.sukhachov@gmail.com>
2025-11-22 22:04:04 +0200

    adding vrew mamber info

--- Moonshot/MissionView.swift
+++ Moonshot/MissionView.swift
@@ -8,7 +8,12 @@
 import SwiftUI
 
 struct MissionView: View {
+    struct CrewMember {
+        let role: String
+        let astronaut: Astronaut
+    }
     let mission: Mission
+    let crew: [CrewMember]
     var body: some View {
         ScrollView {
             VStack {
@@ -26,6 +31,33 @@ struct MissionView: View {
                         
                 }
                  .padding(.horizontal)
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack {
+                        ForEach(crew, id: \.role) { crewMember in
+                            NavigationLink {
+                                Text("Astronauts Details")
+                            } label: {
+                                HStack {
+                                    Image(crewMember.astronaut.id)
+                                        .resizable()
+                                        .frame(width: 104, height: 72)
+                                        .clipShape(.capsule)
+                                        .overlay(
+                                            Capsule().strokeBorder(.white, lineWidth: 1)
+                                         )
+                                    VStack(alignment: .leading) {
+                                         Text(crewMember.astronaut.name)
+                                            .font(.headline)
+                                            .foregroundStyle(.white)
+                                        Text(crewMember.role)
+                                            .foregroundStyle(.secondary)
+                                    }
+                                }
+                                .padding(.horizontal)
+                            }
+                        }
+                    }
+                }
             }
             .padding(.bottom)
         }
@@ -33,10 +65,22 @@ struct MissionView: View {
         .navigationBarTitleDisplayMode(.inline)
         .background(.darkBackground )
     }
+    init(mission: Mission, astronauts: [String: Astronaut]) {
+        self.mission = mission
+        self.crew = mission.crew.map { member in
+            if let astronaut = astronauts[member.name] {
+                return CrewMember(role: member.role, astronaut: astronaut)
+            }
+            else {
+                fatalError("Missingg \(member.name)")
+            }
+        }
+    }
 }
 
 #Preview {
     let missions: [Mission] = Bundle.main.decode("missions.json")
-    return MissionView(mission: missions[0])
+    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    return MissionView(mission: missions[0], astronauts: astronauts)
         .preferredColorScheme(.dark)
 }
